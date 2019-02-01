ce82ded77763d08fb519f3e054a0697caf516cfe
Kubilay <kubilay__1903@hotmail.com>
2019-02-01 23:24:43 +0300

    ssdf

--- BLL.Hotel/Repositories/PersonnelRepository.cs
+++ BLL.Hotel/Repositories/PersonnelRepository.cs
@@ -61,9 +61,45 @@ namespace BLL.Hotel.Repositories
             return ent.Personnel.ToList();
         }
 
+        
         public bool UpdatePersonnel(Personnel p)
         {
-            throw new NotImplementedException();
+            bool Sonuc = false;
+            try
+            {
+                ent.SaveChanges();
+                Sonuc = true;
+            }
+            catch (Exception ex)
+            {
+                string hata = ex.Message;
+            }
+            return Sonuc;
+        }
+        public bool PersonnelControl(Personnel p)
+        {
+            var Personel = (from per in ent.Personnel
+                            where per.PersonName == p.PersonName
+                            select per).FirstOrDefault();
+            if (Personel != null)
+                return true;
+            return false;
+        }
+        public Personnel GetPersonnelById(int ID)
+        {
+            Personnel degisen = (from p in ent.Personnel
+                                 where p.Id == ID
+                                 select p).FirstOrDefault();
+            return degisen;
+        }
+        public bool PersonnelControlFromUpdate(Personnel p)
+        {
+            var personel = (from ext in ent.ExtraTypes
+                            where ext.Type == p.PersonName && ext.Id != p.Id
+                            select ext).FirstOrDefault();
+            if (personel != null)
+                return true;
+            return false;
         }
     }
 }
